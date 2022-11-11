96b60e4e694a7666f06d002fdf7dbdf4975288d1
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2022-11-10 22:48:55 -0300

    organizing folders

--- server/server.ts
+++ server/server.ts
@@ -2,5 +2,10 @@ import express from 'express'
 
 const app = express()
 
+import router from './routes'
 
-app.listen(3333, () => console.log('server is running...'))
+app.use(router)
+
+app.listen(3333, () => {
+  console.log('Server is running at http://localhost:3333')
+})
