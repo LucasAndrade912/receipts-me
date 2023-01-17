f17268aa0f783cd627dc44d9541a597353080f6e
Lucas Andrade <lucasantos.dev@gmail.com>
2023-01-17 13:23:48 -0300

    style bottom tab navigator

--- mobile/routes/drawer.routes.tsx
+++ mobile/routes/drawer.routes.tsx
@@ -1,13 +1,19 @@
+import { Text } from 'react-native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import { TabsRoutes } from './tabs.routes'
 
 const { Navigator, Screen } = createDrawerNavigator()
 
+function ReceiptsFavorites() {
+  return <Text>Receitas Favoritas</Text>
+}
+
 export function DrawerRoutes() {
   return (
     <Navigator initialRouteName="home" screenOptions={{ headerShown: false }}>
-      <Screen name="home" component={TabsRoutes} />
+      <Screen name="home" component={TabsRoutes} options={{ drawerLabel: 'Home' }} />
+      <Screen name="receiptsFavorites" component={ReceiptsFavorites} options={{ drawerLabel: 'Receitas Favoritas' }} />
     </Navigator>
   )
 }
--- mobile/routes/tabs.routes.tsx
+++ mobile/routes/tabs.routes.tsx
@@ -1,10 +1,21 @@
 import { Text } from 'react-native'
+import { ForkKnife, MagnifyingGlass } from 'phosphor-react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-const { Navigator, Screen } = createBottomTabNavigator()
+type BottomTabNavigatorParamList = {
+  receipts: undefined
+  receipt: undefined
+  search: undefined
+}
+
+const { Navigator, Screen } = createBottomTabNavigator<BottomTabNavigatorParamList>()
 
 function Receipts() {
   return <Text>Receitas</Text>
+}
+
+function Receipt() {
+  return <Text>Receita linda</Text>
 }
 
 function Search() {
@@ -13,8 +24,26 @@ function Search() {
 
 export function TabsRoutes() {
   return (
-    <Navigator initialRouteName="receipts" screenOptions={{ headerShown: false }}>
+    <Navigator
+      initialRouteName="receipts"
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          height: 72,
+          backgroundColor: '#D64122'
+        },
+        tabBarIcon: ({ focused }) => {
+          const color = focused ? '#FFF' : '#D8D3D3'
+
+          return route.name === 'receipts'
+            ? <ForkKnife size={32} color={color} weight="regular" />
+            : <MagnifyingGlass size={32} color={color} weight="bold" />
+        }
+      })}
+    >
       <Screen name="receipts" component={Receipts} />
+      <Screen name="receipt" component={Receipt} options={{ tabBarButton: () => null }} />
       <Screen name="search" component={Search} />
     </Navigator>
   )
